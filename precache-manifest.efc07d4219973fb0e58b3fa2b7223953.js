3780fccfc9c1dbb649f8778c029650e7ad970edf
TEst <test@test.com>
2022-01-01 23:47:37 -0600

    Updates

---
+++ precache-manifest.efc07d4219973fb0e58b3fa2b7223953.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a853a539730aef9838c6bcb3048d948e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d5bf3b21595971f2a145",
+    "url": "./static/css/main.bf0095ea.chunk.css"
+  },
+  {
+    "revision": "9e76de35d71e578643ac",
+    "url": "./static/js/2.4ee44813.chunk.js"
+  },
+  {
+    "revision": "d5bf3b21595971f2a145",
+    "url": "./static/js/main.103674cf.chunk.js"
+  },
+  {
+    "revision": "70daa8214f56383324b2",
+    "url": "./static/js/runtime-main.288978b6.js"
+  }
+]);
