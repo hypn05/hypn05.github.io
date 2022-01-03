cb0f79a9541df90d7d7e3fc5c22da5edd46763c5
Abbas Ali <abali@tesla.com>
2022-01-02 23:33:32 -0600

    spelling update

--- src/components/cat.jsx
+++ src/components/cat.jsx
@@ -65,9 +65,9 @@ export class Cat extends Component {
             term: "",
             description: [
               "Job functions include threat modeling, bug bounty triage, pentest coordination, vulnerability tracking, security reviews of internal and external products.",
-              "Build out Application Security program (SAST, DAST, SCA, Developer Education, & Policies)",
-              "Setup and Manage container security tooling",
-              "Collaborate frequently with different engineering teams to identify and address security issues",
+              "Build out Application Security program (SAST, DAST, SCA, Developer Education, & Policies).",
+              "Setup and Manage container security tooling.",
+              "Collaborate frequently with different engineering teams to identify and address security issues.",
             ],
           },
         ],
